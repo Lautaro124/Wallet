275171a77b92874b7b57e8cc511971228b30ef3a
lautaro.gonzalez <lautaro.gonzalez@ctl.com.ar>
2022-10-21 00:25:15 -0300

    Atoms added

---
+++ src/components/atoms/text/TextBase.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const TextBase = () => {
+  return (
+    <View>
+      <Text>TextBase</Text>
+    </View>
+  )
+}
+
+export default TextBase
+
+const styles = StyleSheet.create({})
