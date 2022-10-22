89f27acc375156cfcc42266987320be0ebc908e7
lautaro.gonzalez <lautaro.gonzalez@ctl.com.ar>
2022-10-21 21:13:40 -0300

    Text

--- src/components/atoms/text/TextBase.tsx
+++ src/components/atoms/text/TextBase.tsx
@@ -1,14 +1,47 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text } from 'react-native'
+import { type PropsWithChildren, FC } from 'react'
 
-const TextBase = () => {
-  return (
-    <View>
-      <Text>TextBase</Text>
-    </View>
-  )
+type PropsTextBase = {
+  type?: 'title' | 'subtitle' | 'small' | 'large';
+  weight?: 'bold' | 'italic';
+  styleProp?: { [key: string]: string| number };
+}
+
+const TextBase: FC<PropsWithChildren<PropsTextBase>> = ({ children, styleProp, type, weight }) => {
+  const styles = [
+    style.body,
+    [ type && style[ type ] ],
+    [ weight && style[ weight ] ],
+    styleProp
+  ]
+
+  return <Text style={ styles }>{ children }</Text>
 }
 
 export default TextBase
 
-const styles = StyleSheet.create({})
+const style = StyleSheet.create({
+  body: {
+    fontSize: 16,
+  },
+  title: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  subtitle: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  small: {
+    fontSize: 14,
+  },
+  large: {
+    fontSize: 16,
+  },
+  bold: {
+    fontWeight: 'bold'
+  },
+  italic: {
+    fontStyle: 'italic',
+  },  
+})
